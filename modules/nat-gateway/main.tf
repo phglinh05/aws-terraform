523e342bd3dd9b12a72722706a2fe27142056bf0
linhphuongbae <linhphuongtranltv@gmail.com>
2026-06-03 12:21:36 +0700

    add terraform modules: vpc, nat-gateway, security-groups, ec2

---
+++ modules/nat-gateway/main.tf
@@ -0,0 +1,10 @@
+resource "aws_eip" "nat_eip" {
+    domain = "vpc"
+    tags = {Name = "NAT-EIP"}
+}
+
+resource "aws_nat_gateway" "nat" {
+    allocation_id = "aws_eip.nat_eip.id"
+    subnet_id = var.public_subnet_id
+    tags = {Name = "NAT"}
+}
---
+++ modules/nat-gateway/output.tf
@@ -0,0 +1,1 @@
+output "nat_gw_id" {value = aws_nat_gateway.nat.id}
